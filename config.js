0a920673e7643d87f42b613b112c0eb32cb601a2
Giridhar pai <132196481+Giri227@users.noreply.github.com>
2024-07-27 19:51:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    titleWeb: "DO YOU STILL LOVE ME??",
+    introTitle: 'RIYA',
+    introDesc: `Lost in my lonely heart`,
+    btnIntro: '^^HiHi^^',
+    title: 'I want to make things right.🥰',
+    desc: 'Do you love me?',
+    btnYes: 'I LOVE YOU TOO...... <33',
+    btnNo: 'NO',
+    question: 'Why do you love me <3',
+    btnReply: 'Sent to you <3',
+    reply: 'Love you so much 😘😘',
+    mess: 'I WANNA MEET YOU SOON TO CONFESS.',
+    btnAccept: 'Okiiiii I LOVE U RIYA <3',
+    messLink: '' //link mess của các bạn
+  }
